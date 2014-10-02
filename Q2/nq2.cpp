94c48be08977d157a663e15aff6f974ccd55c2d0
Nicholash Bedi <nbedi@uwaterloo.ca>
2014-10-02 12:09:24 -0400

    Done functions

--- Q2/nq2.cpp
+++ Q2/nq2.cpp
@@ -12,9 +12,30 @@ double convDeg(int deg, int mins)
 	return deg + double(mins)/60;
 }
 
+double toRadians(double degrees)
+{
+	//2pi =360degree
+	return ( degrees * M_PI / 180.0  ) ;
+}
+
+double Xcoor(double lat, double lon)
+{
+	return ( 111 * lon * cos( toRadians(lat) ) );
+}
+
+double Ycoor(double lat)
+{
+	return ( 111 * lat );
+}
+
 int main()
 {
-	cout << convDeg(60,30) << endl;
+	ifstream coord("message.txt");
+	if (!coord)
+	{
+		cout << "unable to open file" << endl;
+		return EXIT_FAILURE;
+	}
 	
 	return EXIT_SUCCESS;
 }
