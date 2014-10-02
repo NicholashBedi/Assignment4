e5236dde17d078313baed1d04f2167aba84ac712
Nicholash Bedi <nbedi@uwaterloo.ca>
2014-10-01 22:02:13 -0400

    Q1

---
+++ Q1/Nq1.cpp
@@ -0,0 +1,48 @@
+//Nicholash Bedi
+//Assignment 4 question 1
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<iomanip>
+#include<cstdlib>
+using namespace std;
+
+
+//RANDOM CODE
+int main()
+{
+	double serviceCharge = 7.50, perkm = 3.25,x,y,dy,dx,distance = 0.0;
+	int numstops;
+	char NS, WE;
+//	ifstream routes("taxiDir_rev.txt");
+	ifstream routes("text.txt");
+	
+	if(!routes)
+	{
+		cout << "Unable to open file" << endl;
+		return EXIT_FAILURE;
+	}
+	
+	
+	while((routes >> numstops) != false)
+	{
+		for(int i = 0; i < numstops; i++)
+		{
+			routes >> dy;
+			routes >> NS;
+			if(NS == 'S')
+				dy*=-1;
+			cout << dy << endl;
+			
+			routes >> dx;
+			routes >> WE;
+			if(WE == 'E')
+				dx *= -1;
+			cout << dx << endl;
+		}
+		cout << endl;
+	}
+	
+	return EXIT_SUCCESS;
+	
+}
