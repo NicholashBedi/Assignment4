b227eccb2d4d21cc7e36be0ff3f642a39580abed
Nicholash Bedi <nbedi@uwaterloo.ca>
2014-10-02 20:45:12 -0400

    Complete, checking tests would be good

--- Q2/nq2.cpp
+++ Q2/nq2.cpp
@@ -12,10 +12,14 @@ double convDeg(int deg, int mins)
 	return deg + double(mins)/60;
 }
 
-double toRadians(double degrees)
+double toRadians(long double degrees)
 {
-	//2pi =360degree
 	return ( degrees * M_PI / 180.0  ) ;
+}
+
+double toDegrees(double radians)
+{
+	return radians / M_PI * 180;
 }
 
 double Xcoor(double lat, double lon)
@@ -36,6 +40,53 @@ int main()
 		cout << "unable to open file" << endl;
 		return EXIT_FAILURE;
 	}
+	
+	
+	int initDegIntLat, initDegMinLat, initDegIntLon, initDegMinLon, numSites;
+	coord 	>> initDegIntLat >> initDegMinLat
+			>> initDegIntLon >> initDegMinLon
+			>> numSites;
+	 
+	double 	initDegLat = convDeg(initDegIntLat, initDegMinLat),
+			initDegLon = convDeg(initDegIntLon, initDegMinLon);
+	
+	double 	initX = Xcoor(initDegLat, initDegLon),
+			initY = Ycoor(initDegLat);
+	
+	int DegIntLat,DegMinLat,DegIntLon,DegMinLon;
+	double Lat, Lon, dx, dy, angle; 
+	char NS,WE;
+	for (int i = 0; i < numSites; i++)
+	{
+		coord 	>> DegIntLat >> DegMinLat
+				>> DegIntLon >> DegMinLon;
+		Lat = convDeg(DegIntLat, DegMinLat);
+		Lon = convDeg(DegIntLon, DegMinLon);
+		dx = Xcoor(Lat, Lon) - initX;
+		dy = Ycoor(Lat) - initY;
+		cout << dx << " " << dy << endl << endl;
+		angle = toDegrees(atan2(dx,dy));
+		cout << "Angle: " << angle << endl;
+		
+		if (angle == 0)
+			cout << "North (0 degrees)" << endl;
+		else if (angle == 90)
+			cout << "East" << endl;
+		else if (angle == -90)
+			cout << "West";
+		else if (angle == -180)
+			cout << "South" << endl;	
+		else if (angle > -90 && angle < 0)
+			cout << "North " << fabs(angle) << " degrees West" << endl;
+		else if (angle < 90 && angle > 0)
+			cout << "North " << angle << " degrees East" << endl;
+		else if (angle < 180 && angle > 90)
+			cout << "South " << 180-angle << " degrees East" << endl;
+		else if (angle < -90)
+			cout << "South " << angle -180 << " degrees West" << endl;
+
+	}
+	 
 	
 	return EXIT_SUCCESS;
 }
